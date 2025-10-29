2ef5ca1f2fe8de406d98e06e66825498a6ffa28e
Nick Clegg <nac542@gmail.com>
2025-10-29 17:22:16 -0400

    Refactor testing fixtures, add message type (#93)
    * Refactor testing fixtures, add message type
    
    * Address pr feedback
    
    * Address merge conflict

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ export type {
   ModelContentBlockStartEvent,
   TextDelta,
   ToolUseInputDelta,
-  ReasoningDelta,
+  ReasoningContentDelta,
   ContentBlockDelta,
   ModelContentBlockDeltaEvent,
   ModelContentBlockStopEvent,
